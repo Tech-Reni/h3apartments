ce1c7913ab21729e62806204ec9362326cfd6973
Tech-Reni <techren123@gmail.com>
2025-11-17 15:28:07 -0500

    Initial Commit

---
+++ js/shortlets.js
@@ -0,0 +1,85 @@
+const shortletsData = {
+    "listings": [
+        {
+            "id": "h3-downtown",
+            "title": "DOWN TOWN SUITE",
+            "price": "₦150,000 / Night",
+            "rooms": "1 Bedroom",
+            "location": "Onike Iwaya, Lagos",
+            "rating": "⭐ 4.9",
+            "rentMessage": "Hello, I am interested in a shortlet. Can you share more details?",
+            "images": [
+                "img/downtown/master-room.jpg",
+                "img/downtown/front_door.jpg",
+                "img/downtown/kitchen.jpg",
+                "img/downtown/toilet-1.jpg",
+            ],
+            "amenities": [
+                "Free WiFi",
+                "Air Conditioning",
+                "Smart TV",
+                "Kitchen",
+                "24/7 Power Supply",
+                "Parking Space"
+            ]
+        },
+        {
+            "id": "h3-prestige",
+            "title": "PRESTIGE SUITE",
+            "price": "₦100,000 / Night",
+            "rooms": "2 Bedroom ",
+            "location": "Onike Iwaya, Lagos",
+            "rating": "⭐ 4.8",
+            "rentMessage": "Hello, I am interested in a shortlet. Can you share more details?",
+            "images": [
+                "img/prestige/sitting-room-1.jpg",
+                "img/prestige/kitchen.jpg",
+                "img/prestige/sitting-room-2.jpg",
+                "img/prestige/bedroom-1.jpg",
+                "img/prestige/sitting-room-3.jpg",
+                "img/prestige/bedroom-2.jpg",
+                "img/prestige/toilet-1.jpg",
+                "img/prestige/bedroom-3.jpg",
+                "img/prestige/toilet-2.jpg",
+                "img/prestige/front_door.jpg"
+            ],
+            "amenities": [
+                "Free WiFi",
+                "Air Conditioning",
+                "Smart TV",
+                "Kitchen",
+                "24/7 Power Supply",
+                "Parking Space"
+            ]
+        },
+        {
+            "id": "h3-elite",
+            "title": "PRESTIGE SUITE",
+            "price": "₦100,000 / Night",
+            "rooms": "2 Bedroom ",
+            "location": "Onike Iwaya, Lagos",
+            "rating": "⭐ 4.8",
+            "rentMessage": "Hello, I am interested in a shortlet. Can you share more details?",
+            "images": [
+                "img/elite/sitting-room-1.jpg",
+                "img/elite/kitchen.jpg",
+                "img/elite/sitting-room-2.jpg",
+                "img/elite/bedroom-1.jpg",
+                "img/elite/sitting-room-3.jpg",
+                "img/elite/bedroom-2.jpg",
+                "img/elite/toilet-1.jpg",
+                "img/prestige/bedroom-3.jpg",
+                "img/prestige/toilet-2.jpg",
+                "img/prestige/front_door.jpg"
+            ],
+            "amenities": [
+                "Free WiFi",
+                "Air Conditioning",
+                "Smart TV",
+                "Kitchen",
+                "24/7 Power Supply",
+                "Parking Space"
+            ]
+        },
+    ]
+}; 
